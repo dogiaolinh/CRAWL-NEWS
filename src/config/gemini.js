8caed39db41eb81a147869a831209636a56149f8
DNP Tech <tech.dnp@baohiem.app>
2025-12-27 23:37:45 +0700

    tep

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,5 +1,10 @@
 // src/config/gemini.js
 const API_KEYS = [
+  "AIzaSyCHiLWe7nI6d81dJKwjXog41bhf3Dr7E0E",
+  "AIzaSyDH1kaG-srGtFVrN9vcQQlyRdFm-UJlw4w",
+  "AIzaSyBVpoD2C3e6lg_Cgq1ecqqi7YKq2rixsJg",
+  "AIzaSyBBHoZaBypNWBcGJ-BFaW-5W5_kUPqemAY",
+
   "AIzaSyCNpHtusiYak5L_3XGiNIvMCpmd5imD4bI",
   "AIzaSyCJajYCYyXrHEVwxOTgyKRMW8hzevahxkg",
   "AIzaSyBNojf23sAZ3SnXAXyfR4qYBygwPImo1uk",
