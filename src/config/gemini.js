3419fdf7728181196d71560d099dd38623d7f4db
DNP Tech <tech.dnp@baohiem.app>
2026-01-04 10:41:50 +0700

    add token

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,5 +1,10 @@
 // src/config/gemini.js
+// debel , ku, setup , dglinh2
 const API_KEYS = [
+  "AIzaSyC3oD48RqZnyqCUy0kJQlVSbllGr8xcvAs",
+  "AIzaSyCqnF_LUXCn2885FtHVHp5pJlSTUu1P-mY",
+  "AIzaSyCegmt5zVysPYBQxQzXyuxdkgTEp0cW-Y8",
+  "AIzaSyB4nJJxoC53UA6EzQA-2hMsMmf0ZMKXpDs",
   "AIzaSyCHiLWe7nI6d81dJKwjXog41bhf3Dr7E0E",
   "AIzaSyDH1kaG-srGtFVrN9vcQQlyRdFm-UJlw4w",
   "AIzaSyBVpoD2C3e6lg_Cgq1ecqqi7YKq2rixsJg",
